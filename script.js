2cd7a8d5ff831eb547b1ba8d2211f4244df0c560
Alphonce Mutai <mutaialphonce1@gmail.com>
2025-02-08 15:16:20 +0300

    carousel update

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
         this.querySelector(".dropdown-menu").style.display = "none";
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel");
+    
+    let index = 0;
+    function slideImages() {
+        index++;
+        if (index > 3) index = 0; // Reset after last image
+        carousel.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(slideImages, 2000); // Change image every 2 seconds
+});
